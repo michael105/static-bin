198241d956ebcb6d1fa96a13b75b7cd3d6729a1a
Michael Myer <misc.myer@zoho.com>
2023-10-25 13:22:46 +0000

    Add binaries

--- bak/blake3sums.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-	echo -e " blake3 \n--------\n" 
-	#for i in `find . -maxdepth 1 -type f -executable -name '??*' -exec basename {} \; | sort`
-	for i in `find . -maxdepth 2 -type f -executable -name '??*' | sort`
-	do 
-		cat $i | ./blake3sum | tr -d "\n" 
-		echo "   $i"; 
-	done 
