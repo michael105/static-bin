511c6eac3eafa05902213dd7f1aa5cf46b76a278
misc147 <misc7@posteo.net>
2024-10-15 16:59:27 +0000

    Add binaries

--- blake3sums.sh
+++ blake3sums.sh
@@ -4,6 +4,6 @@
 	#for i in `find . -maxdepth 1 -type f -executable -name '??*' -exec basename {} \; | sort`
 	for i in `find . -maxdepth 2 -type f -executable -name '??*' | sort`
 	do 
-		cat $i | ./blake3sum | tr -d "\n" 
+		cat $i | blake3sum | tr -d "\n" 
 		echo "   $i"; 
 	done 
