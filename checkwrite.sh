ed2899a5fc988f8cf0bfbdae408a8ef8c0adca02
misc147 <misc147@disroot.org>
2026-09-25 00:35:22 +0000

    remove. update.

--- checkwrite.sh
+++ checkwrite.sh
@@ -1,1 +1,1 @@
-for i in `find . -maxdepth 2 -type f -executable -name '??*' | sort`; do ( cat $i | ./$1"sum" | tr -d '\n'; echo " $i"); done > checksums.$1.txt
+for i in `find . -maxdepth 2 -type f -executable -name '??*' | sort`; do ( cat $i | ./$1"sum" | tr -d '\n'; echo "  $i"); done > checksums.$1.txt
